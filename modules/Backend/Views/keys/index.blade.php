f24c5b9f335c58fb4b52812ff8b9c5a0471fe134
phandung1511 <phandung@giaxaydung.com>
2016-08-02 17:40:03 +0700

    Phan Dung commit

--- modules/Backend/Views/keys/index.blade.php
+++ modules/Backend/Views/keys/index.blade.php
@@ -19,13 +19,7 @@
                             @if (!empty($products))
                                 <div class="has-feedback">
                                     <div class="col-sm-12">
-                                        <div class="col-sm-4 no-padding">
-                                            <select name="status" class="form-control  input-sm">
-                                                <option value="1">Key thương mại</option>
-                                                <option value="0">Key dùng thử</option>
-                                            </select>
-                                        </div>
-                                        <div class="col-sm-4 no-padding">
+                                        <div class="col-sm-6 no-padding">
                                             <select name="category" class="form-control  input-sm">
                                                 @foreach($products as $c)
                                                     <option value="{!! $c->id !!}"
@@ -35,7 +29,7 @@
                                                 @endforeach
                                             </select>
                                         </div>
-                                        <div class="col-sm-4 no-padding">
+                                        <div class="col-sm-6 no-padding">
                                             <input type="text" class="form-control input-sm" name="keyword" placeholder="@lang('courses.search')" value="{!! isset($keyword) ? $keyword : '' !!}">
                                             <span class="glyphicon glyphicon-search form-control-feedback"></span>
                                         </div>
@@ -57,74 +51,24 @@
 				    <div class="col-md-1 padding-left-5">
 					<button type="button" class="btn btn-default btn-sm checkbox-toggle"><i class="fa fa-square-o"></i></button>
 				    </div>
-				    <div class="col-md-3">
+				    <div class="col-md-4">
 					@lang('key.name')
 				    </div>
-				    <div class="col-md-2">
+				    <div class="col-md-3">
 					@lang('key.date')
 				    </div>
 				    <div class="col-md-2">
-					@lang('key.com_1')
+					@lang('key.com')
 				    </div>
 				   
 				    <div class="col-md-2">
-					@lang('key.com')
-				    </div> 
-                                     <div class="col-md-2">
-					@lang('key.function')
+					@lang('key.com_1')
 				    </div> 
 				</div>
 			    </div>
 			</div>
 			<!-- /.header-list -->
-                        <!-- .content-list -->
-                            <div class="row-group content-list">
-                                @if(!empty($keys) && $keys->count() > 0)
-                                @foreach($keys as $key)
-                                <div class="row item">
-                                    <div class="col-md-12">
-                                        <div class="col-md-1 padding-left-10">
-                                            <input type="checkbox" 
-                                                    init="iCheck"
-                                                            data-icheck-class="icheckbox_flat-blue"
-                                                            data-increase-area="20%"
-                                                            value="{!! $key->id !!}">
-                                        </div>
-                                        <div class="col-md-3">
-                                            <a href="{!! URL::action('\Backend\Controllers\CategoriesController@edit', array('id' => $key->id))!!}">{!! $key->license_serial !!}</a>
-                                        </div>
-                                        <div class="col-md-2">
-                                            {!! $key->type_expire_date !!}
-                                        </div>
-                                        <div class="col-md-2">
-                                            {!! $key->license_is_registered !!}
-                                        </div>
-                                        <div class="col-md-2">
-                                            {!! $key->license_no_computers !!}
-                                        </div>
-                                       
-                                        <div class="col-md-2">
-                                        <a href="{!! URL::action('\Backend\Controllers\KeyController@edit', array('id' => $key->id)) !!}"
-										   init="tooltip"
-										   title="@lang('common.edit')"
-										   class="btn btn-success btn-sm">
-                                            <i class="fa fa-edit"></i>
-                                        </a>
-                                        <button init="tooltip"
-												title="@lang('common.delete')"
-                                            class="btn btn-danger btn-sm"
-                                            data-toggle="modal" data-target="#modal-delete-{!! $key->id !!}">
-                                            <i class="fa fa-trash"></i>  
-                                        </button>
-
-                                    </div> 
-                                    </div>
-                                </div>
-                                @endforeach
-
-                                @endif
-                            </div>
-                            <!-- /.content-list -->                                      
+                    
                 </div>
                 
             </div>
---
+++ resources/lang/vi/customers.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+    'customers' => 'Khách hàng',
+    'searchCategories' => 'Tìm kiếm danh mục khóa học',
+    'enterInfo' => 'Nhập thông tin danh mục khóa học',
+    'infoCate' => 'Thông tin danh mục',
+    'list' => 'Danh sách khách hàng',
+    'add' => 'Thêm mới danh mục',
+    'edit' => 'Chỉnh sửa danh mục',
+    'name' => 'Tên danh mục',
+    'description' => 'Mô tả',
+    'image' => 'Hình ảnh',
+    'status' => 'Trạng thái',
+    'order' => 'Thứ tự',
+    'delete' => 'Xóa danh mục',
+    'reload' => 'Tải lại danh sách danh mục',
+    'infoCourse' => 'Thông tin khóa học',
+    'course' => 'Khóa học',
+    'selectCourse' => 'Chọn khóa học',
+    'caption' => 'Chú thích',
+    'cfDelTit' => 'Xóa Danh mục khóa học',
+    'cfDelMsg' => 'Bạn có chắc chắn muốn xóa danh mục khóa học này?',
+];
