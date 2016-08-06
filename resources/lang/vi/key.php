cd852a46029734a388fe7a27b7f18c22d59e0b2e
Vietanh0412 <vietanh@gxd.vn>
2016-08-06 08:59:40 +0700

    lll

--- resources/lang/vi/key.php
+++ resources/lang/vi/key.php
@@ -11,9 +11,12 @@ return [
     'function'=>'Chức năng',
     'search'=>'Tìm kiếm',
 <<<<<<< HEAD
+<<<<<<< HEAD
     'try'=>'Key dùng thử',
     'sale'=>'Key thương mại',
     'function'=>'Chức năng'
+=======
+>>>>>>> parent of 1d0d2d6... key
 =======
 >>>>>>> parent of 1d0d2d6... key
 ];
